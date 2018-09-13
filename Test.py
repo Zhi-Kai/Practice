af1c81e23ec188c14c26701f8788e8eba71b2dd0
Kyle_C <waheroc9527@outlook.com>
2018-09-13 19:51:04 +0800

    Guess game

--- Test.py
+++ Test.py
@@ -1,20 +1,11 @@
-#<<<<<<< HEAD
-#<<<<<<< HEAD
-print("Hello world!")
-#=======
-
-for i in range(1,10):
-    for j in range(1,10):
-        print(i,"x",j,"=",i*j,end=' ')
-    print(end='\n')
-
-#>>>>>>> 9x9_local
-#=======
-Sum = 0
-Cont = 0
-while Cont<100 :
-    Cont += 1
-    Sum += Cont
-print(Sum)
-
-#>>>>>>> 1addto100
+import random
+ans = random.randint(1,100)
+for i in range(1,5):
+    print("Please entry the answer!")
+    player_ans = int(input())
+    if player_ans == ans:
+        print("Bingo!")
+        break
+    else:
+        print("you are wrong~")
+print("Game over",'\n',"The andswer is",ans)
