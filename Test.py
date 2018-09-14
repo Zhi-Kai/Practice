51f2da1393b5df06f11cf3acf1e98c609f0fd9f0
Kyle_C <waheroc9527@outlook.com>
2018-09-14 13:51:06 +0800

    Import .json file

--- Test.py
+++ Test.py
@@ -1,13 +1,1 @@
-import random
-ans = random.randint(1,100)
-for i in range(1,5):
-    print("Please entry the answer!")
-    player_ans = int(input())
-    if player_ans == ans:
-        print("Bingo!")
-        break
-    else:
-        print("you are wrong~")
-print("Game over",'\n',"The andswer is",ans)
-
-
+# Import .json file
