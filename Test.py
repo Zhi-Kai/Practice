64020aa9d61bf02e578512bbe868a57569c9f4aa
Kyle_C <waheroc9527@outlook.com>
2018-09-14 14:27:12 +0800

    read *.json file

--- Test.py
+++ Test.py
@@ -1,1 +1,34 @@
 # Import .json file
+import json
+#=================================================#
+# Reading data example
+
+# my_list = []
+# with open('naming base.json', 'r', encoding="utf8") as read_f:
+#     for row in read_f.readlines():
+#         # strip("\n") 去除換行符號
+#         my_list.append(row.strip('\n'))
+ 
+# for data in my_list:
+#     print(data)
+
+
+
+#=================================================#
+# unsafety
+# basefile = open('naming base.json','r',encoding='utf8')
+# Database = json.loads(basefile.read())
+# print(Database)
+
+
+
+#=================================================#
+#safety
+with open('naming base.json','r',encoding="utf8") as basefile:
+    Database = json.loads(basefile.read())
+print(Database)
+
+
+
+
+
