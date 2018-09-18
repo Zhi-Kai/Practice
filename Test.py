c41dc37b44e2ed857bc52fa606f9603106f860f5
Kyle_C <waheroc9527@outlook.com>
2018-09-18 14:50:01 +0800

    Get value

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
 # Import .json file
 import json
+import codecs
 #=================================================#
 # Reading data example
 
@@ -24,11 +25,44 @@ import json
 
 #=================================================#
 #safety
+#using read() to transfer *.json file to string
 with open('naming base.json','r',encoding="utf8") as basefile:
-    Database = json.loads(basefile.read())    
-print(Database)
+    Database = json.loads(basefile.read())
+    Date = json.dumps(basefile.read())    
+# print(Database)
+# print(basefile)
 
 #=================================================#
+#codecs application
+#reading *.json file to string
+with codecs.open('naming base.json','r','utf8') as basefile2:
+    Database2 = json.load(basefile2)
+# print(Database2)
+# print (Database2.get("Signal1"))
+# print(Database2["Signal1"])
+
+# get value from *.json file
+Datebace2_1 = Database["Signal1"]
+print(Datebace2_1["Function_group"])
+#=================================================#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
