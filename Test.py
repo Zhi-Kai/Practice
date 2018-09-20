9b9ec22ff1179889b90949c3b442af7ec84704ce
Kyle_C <waheroc9527@outlook.com>
2018-09-20 14:53:34 +0800

    add exception

--- Test.py
+++ Test.py
@@ -1,13 +1,31 @@
+# import random
+# ans = random.randint(1,100)
+# for i in range(1,5):
+#     print("Please entry the answer!")
+#     player_ans = int(input())
+#     if player_ans == ans:
+#         print("Bingo!")
+#         break
+#     else:
+#         print("you are wrong~")
+# print("Game over",'\n',"The andswer is",ans)
+
+#=====================================================
+#try except
 import random
 ans = random.randint(1,100)
-for i in range(1,5):
+
+for i in range(0,5,1):
     print("Please entry the answer!")
-    player_ans = int(input())
-    if player_ans == ans:
-        print("Bingo!")
-        break
-    else:
-        print("you are wrong~")
+    try:
+        player_ans = int(input())
+
+        if player_ans == ans:
+            print("Bingo!")
+            break
+        else:
+            print("you are wrong~")
+    except  ValueError:
+        i = i+1
+        print("Please entry a number!")
 print("Game over",'\n',"The andswer is",ans)
-
-
