b9a7513f440c964dae4d8b9c73721516f5204517
Kyle_C <waheroc9527@outlook.com>
2018-09-25 13:17:30 +0800

    Frame test successfully

--- Test.py
+++ Test.py
@@ -1,9 +1,3 @@
-from sys import version_info
-if version_info.major == 2:
-    import Tkinter as tk
-else:
-    import tkinter as tk
-    
-app = tk.Tk()
-app.title("Hello World")
-app.mainloop()
+import tkinter as tk
+win=tk.Tk()
+win.mainloop()
